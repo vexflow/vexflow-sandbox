3cdc029834bb56c87ecd9f2a1f28aa4404e1c209
Ron B. Yeh <ronyeh@gmail.com>
2025-04-08 13:31:36 -0700

    Add html-minifier.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,28 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+import { minify } from "html-minifier";
+
+// TODO: The GitHub workflow can set an environment variable to turn off debug mode.
+const DEBUG_MODE = true;
+
+function htmlMinifierTransform(content, outputPath) {
+  if (outputPath && outputPath.endsWith(".html")) {
+    const options = DEBUG_MODE
+      ? {}
+      : {
+          // See: https://github.com/kangax/html-minifier?tab=readme-ov-file#options-quick-reference
+          removeComments: true,
+          minifyJS: true,
+          minifyCSS: true,
+          collapseWhitespace: true,
+        };
+    return minify(content, options);
+  } else {
+    return content;
+  }
+}
 
 export default async function (eleventyConfig) {
+  eleventyConfig.addTransform("html-minifier", htmlMinifierTransform);
   eleventyConfig.addGlobalData("currDate", () => new Date());
   eleventyConfig.addPassthroughCopy({ static: "/" });
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
